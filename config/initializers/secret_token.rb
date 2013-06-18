1d5085531406e64a30c0360085b13c633108f9a8
Austin Hansen <austinthansen@gmail.com>
2013-06-17 20:34:57 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nflpool::Application.config.secret_token = '6db38af937c63c7c90c1bf5d7bc8645bb9d18d3cdb93079cea90dd6004cd5414bd18e166f583872b68de05c2fa0927fcfc7728e91693865891bab33dfb508d58'

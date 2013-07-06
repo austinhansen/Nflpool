becbed0e835c6dd682e4766dd31e38baf9814900
austinhansen <austin.t.hansen@gmail.com>
2013-07-06 17:50:32 -0600

    Added names of teams to picks index

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -1,7 +1,9 @@
 class Pick < ActiveRecord::Base
 
   belongs_to :pick_team, class_name: "Team", foreign_key: "pick_team_id"
+  has_one :winning_team, class_name: "Team", foreign_key: "winning_team_id",:through => :game
   belongs_to :user
+  belongs_to :gameid, class_name: "Game", foreign_key: "game_id"
   belongs_to :game
 
 

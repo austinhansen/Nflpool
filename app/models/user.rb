30f8531fd73e05c32903002503305d03006bffdd
austinhansen <austin.t.hansen@gmail.com>
2013-07-16 15:12:27 -0600

    Updated CSS on games index table, improved layout. Added link to Gravatar registration on user edit page

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,7 @@ class User < ActiveRecord::Base
     stripped_email = email.strip
     downcased_email = stripped_email.downcase
     hash = Digest::MD5.hexdigest(downcased_email)
-    "http://gravatar.com/avatar/#{hash}" + "?s=40&d=blank"
+    "http://gravatar.com/avatar/#{hash}" + "?s=40&d=mm"
   end
 
 end

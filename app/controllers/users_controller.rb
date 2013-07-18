58188998733f94e56ab2e6186dae28d523a4f1a9
austinhansen <austin.t.hansen@gmail.com>
2013-07-17 22:18:13 -0600

    Updated game index with 'Spread' column. Updated users controller to allow non-admins to access the leaderboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
-  before_filter :ensure_admin!
+  before_filter :ensure_admin!, except: :leaderboard
 
   def index
     @users = User.scoped

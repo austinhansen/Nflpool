5d67a23a55ad0b7e25a613709a2899b86d5241a1
austinhansen <austin.t.hansen@gmail.com>
2013-07-19 11:09:44 -0600

    Allowed non-admins to see picks index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
-  before_filter :ensure_admin!, except: :leaderboard
+  before_filter :ensure_admin!, except: [:leaderboard, :picks]
 
   def index
     @users = User.scoped

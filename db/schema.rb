c7598699a704bf6c173dbf146adcf0f6bad708f1
Austin Hansen <austinhansen@Austins-MacBook-Air.local>
2013-07-23 21:26:43 -0600

    Changed 'Back' link on leaderboard to a bright green button

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,8 @@ ActiveRecord::Schema.define(version: 20130718155523) do
     t.date     "date"
     t.integer  "team_one_id"
     t.integer  "team_two_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "winning_team_id"
     t.integer  "spread"
   end
@@ -35,20 +35,20 @@ ActiveRecord::Schema.define(version: 20130718155523) do
 
   create_table "teams", force: true do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "profile_name"
-    t.string   "email"
-    t.string   "encrypted_password"
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count"
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -58,5 +58,8 @@ ActiveRecord::Schema.define(version: 20130718155523) do
     t.integer  "points"
     t.boolean  "admin",                  default: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end

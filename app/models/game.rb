a051efdd719bed73ab63bfd4be20bfb33bb5e8af
Austin Hansen <austinthansen@gmail.com>
2013-08-18 10:04:57 -0600

    Removed empty space from game.rb

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,7 +3,4 @@ class Game < ActiveRecord::Base
 	belongs_to :team_two, class_name: "Team", foreign_key: "team_two_id"
 	belongs_to :winning_team, class_name: "Team", foreign_key: "winning_team_id"
 	has_many :picks
-
-
-
 end

805389c635eb8b3e23e86738ea0d80e6d53f567c
Austin Hansen <austinthansen@gmail.com>
2013-08-23 19:09:58 -0600

    Changed time zone to 0 UTC

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,6 @@ module Nflpool
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
     config.beginning_of_week = :tuesday
-    config.time_zone = 'Casablanca'
+    config.time_zone = 'UTC'
   end
 end
